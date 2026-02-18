6dd9fa281d477087666a4aac7f1189fe72665492
Kirill Maltsev <opp.2211@gmail.com>
2026-02-18 19:31:23 +0500

    fix(dev-proxy): не срезать /api при проксировании

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,6 @@ export default defineConfig({
         target: 'http://localhost:8080',
         changeOrigin: true,
         secure: false,
-        rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
   },
